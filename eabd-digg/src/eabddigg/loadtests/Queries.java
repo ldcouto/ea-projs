01c8483e1895fbfbc4e53408b87f80b8db985c25
ldcouto <ldcouto@a326f54a-c8c5-11de-afa3-455a29e9879b>
2009-11-04 17:10:51 +0000

    Adicionado package para tratar dos testes de carga e a classe para albergar as queries

---
+++ eabd-digg/src/eabddigg/loadtests/Queries.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eabddigg.loadtests;
+
+/**
+ *
+ * @author lc
+ */
+public class Queries {
+
+
+
+
+    public void topTenNews(){
+
+    }
+
+    public void recNews(String userNick){
+
+    }
+
+    public void recUsers(String userNick){
+
+    }
+
+    public void allPostsMadeBy(String userNick){
+
+    }
+
+    public void allPostsLikedBy(String userNick){
+        
+    }
+
+}
