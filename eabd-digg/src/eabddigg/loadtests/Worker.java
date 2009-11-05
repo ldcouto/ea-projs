c7b6ec8743b4a48b16e5a5569310cfb7762b045f
ti.veloso <ti.veloso@a326f54a-c8c5-11de-afa3-455a29e9879b>
2009-11-05 19:06:57 +0000

    Adiantada a class worker. Imcompleta.

--- eabd-digg/src/eabddigg/loadtests/Worker.java
+++ eabd-digg/src/eabddigg/loadtests/Worker.java
@@ -5,7 +5,9 @@
 
 package eabddigg.loadtests;
 
+import filehandler.MyFileHandler;
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -20,9 +22,40 @@ public class Worker extends Thread {
     @Override
     public void run() {
         Random r = new Random();
+        ArrayList<String> lines = new ArrayList<String>(counter);
+
+        String filname = this.getClass().getName().toString();
+
+
+        MyFileHandler.createFile(filname);
         while (counter >= 0){
             int action = r.nextInt(12);
+            
+            String randomNick = Queries.selectRandomUser();
+            String randomSlug = Queries.selectRandomUser();
 
+            long start = System.currentTimeMillis();
+
+            switch (action) {
+                case 0 : Queries.topTenNews();
+                         break;
+                case 1 : Queries.topTenFollowers();
+                         break;
+                case 2 : Queries.allFollowersFrom(randomNick);
+                         break;
+                case 3 : Queries.allPostsLikedBy(randomNick);
+                         break;
+                case 4 : Queries.allPostsMadeBy(randomNick);
+                         break;
+                case 5 : Queries.insertVoto(randomSlug, randomNick);
+                         break;
+                case 6 : Queries.loginUser(randomNick);
+                         break;
+                case 7 : Queries.insertNews();
+
+            }
+
+            long end = System.currentTimeMillis();
         }
     }
 
---
+++ eabd-digg/src/filehandler/MyFileHandler.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package filehandler;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author fork
+ */
+public abstract class MyFileHandler {
+
+    /**
+     *
+     * @param filename name of the file, or path to file, to create
+     * @return true if file was created, false if file already exists
+     */
+    public static boolean createFile(String filename) {
+
+        boolean res = false;
+
+        try {
+            File file = new File(filename);
+            res = file.createNewFile();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            return res;
+        }
+    }
+
+    /**
+     * 
+     * @param filename name of the file to edit
+     * @param line line to add to the file
+     */
+    public void writeLine(String filename, String line) {
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(filename,true));
+            out.write(line);
+            out.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeManyLines(String filename, List<String> lines)
+    {
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(filename,true));
+            for (String line : lines) {
+                out.write(line);
+                out.newLine();
+            }
+            out.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+}
