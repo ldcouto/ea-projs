172ec6ebfaab7a893f2483894fd8f32d07851c9b
ti.veloso <ti.veloso@a326f54a-c8c5-11de-afa3-455a29e9879b>
2009-11-09 10:39:24 +0000

    Algumas arestas limadas.

--- eabd-digg/test/ltCodeTests/QueryTests.java
+++ eabd-digg/test/ltCodeTests/QueryTests.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ltCodeTests;
 
 import connectionhandler.MyConnectionHandler;
@@ -12,9 +11,8 @@ import eabddigg.loadtests.Queries;
  *
  * @author lc
  */
-public class QueryTests {
-
-
+public class QueryTests
+{
 
     public static void main(String[] args)
     {
@@ -23,8 +21,8 @@ public class QueryTests {
         ti = System.currentTimeMillis();
         testTopTenNews();
         tf = System.currentTimeMillis();
-        System.out.println("TopTenNews completed in " + (tf-ti) + " ms.");
-/*        System.out.println("Testing TopTenFollowers...");
+        System.out.println("TopTenNews completed in " + (tf - ti) + " ms.");
+        /*        System.out.println("Testing TopTenFollowers...");
         ti = System.currentTimeMillis();
         testTopTenFollowers();
         tf = System.currentTimeMillis();
@@ -47,48 +45,46 @@ public class QueryTests {
         System.out.println("Testing InsertNews...");
         ti = System.currentTimeMillis();
         testInsNews();
-*/
- tf = System.currentTimeMillis();
-        System.out.println("InsertNews completed in" + (tf-ti) + " ms.");
+         */
+        tf = System.currentTimeMillis();
+        System.out.println("InsertNews completed in" + (tf - ti) + " ms.");
     }
 
-        // Aux Functions
-     static public String removeSpecialCharacters(String orig)
-      {
-            String rv;
+    // Aux Functions
+    static public String removeSpecialCharacters(String orig)
+    {
+        String rv;
 
-            // replacing with space allows the camelcase to work a little better in most cases.
-            rv = orig.replace("\\"," ");
-            rv = rv.replace("("," ");
-            rv = rv.replace(")"," ");
-            rv = rv.replace("/"," ");
-            rv = rv.replace("-"," ");
-            rv = rv.replace(","," ");
-            rv = rv.replace(">"," ");
-            rv = rv.replace("<"," ");
-            rv = rv.replace("-"," ");
-            rv = rv.replace("&"," ");
+        // replacing with space allows the camelcase to work a little better in most cases.
+        rv = orig.replace("\\", " ");
+        rv = rv.replace("(", " ");
+        rv = rv.replace(")", " ");
+        rv = rv.replace("/", " ");
+        rv = rv.replace("-", " ");
+        rv = rv.replace(",", " ");
+        rv = rv.replace(">", " ");
+        rv = rv.replace("<", " ");
+        rv = rv.replace("-", " ");
+        rv = rv.replace("&", " ");
 
-            // single quotes shouldn't result in CamelCase variables like Patient's -> PatientS
-            // "smart" forward quote
-            rv = rv.replace("'","");
+        // single quotes shouldn't result in CamelCase variables like Patient's -> PatientS
+        // "smart" forward quote
+        rv = rv.replace("'", "");
 
-            // if you have to find any more weird unicode chars, look here:
-            // http://seth.positivism.org/man.cgi/7/groff_char
-            rv = rv.replace("\u2019",""); // smart forward (possessive) quote.
+        // if you have to find any more weird unicode chars, look here:
+        // http://seth.positivism.org/man.cgi/7/groff_char
+        rv = rv.replace("\u2019", ""); // smart forward (possessive) quote.
 
-            // make sure to get rid of double spaces.
-            rv = rv.replace("   "," ");
-            rv = rv.replace("  "," ");
+        // make sure to get rid of double spaces.
+        rv = rv.replace("   ", " ");
+        rv = rv.replace("  ", " ");
 
-            rv = rv.trim();// Remove leading and trailing spaces.
+        rv = rv.trim();// Remove leading and trailing spaces.
 
-            return(rv);
-      }
+        return (rv);
+    }
 
-    
     // Invoker Methods
-
     private static void testRecNews()
     {
         Queries myQ = new Queries();
@@ -96,31 +92,34 @@ public class QueryTests {
         myQ.recNews(dude);
     }
 
-    private static void testRecUsers() {
+    private static void testRecUsers()
+    {
         Queries myQ = new Queries();
         String dude = removeSpecialCharacters(myQ.selectRandomUser());
         myQ.recUsers(dude);
     }
 
-    private static void testInsVote() {
+    private static void testInsVote()
+    {
         Queries myQ = new Queries();
-        myQ.insertVoto(myQ.selectRandomNews(),myQ.selectRandomUser());
-   }
+        myQ.insertVoto(myQ.selectRandomNews(), myQ.selectRandomUser());
+    }
 
-    private static void testInsNews() {
+    private static void testInsNews()
+    {
         Queries myQ = new Queries();
         myQ.insertNews("banglast");
     }
 
-    private static void testTopTenFollowers() {
+    private static void testTopTenFollowers()
+    {
         Queries myQ = new Queries();
         myQ.topTenFollowers();
     }
 
-    private static void testTopTenNews() {
+    private static void testTopTenNews()
+    {
         Queries myQ = new Queries();
         myQ.topTenNews();
     }
-
-
 }
